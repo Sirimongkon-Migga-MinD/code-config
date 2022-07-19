b5a7072d4b9c2a47c62784df3af9c51285507a0c
Teptat Tangsajakul <teptat.t@ku.th>
2022-07-19 13:37:25 +0700

    code config

---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,18 @@
+package atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+//        DataSourceDB dataSourceDB = new DataSourceDB();
+//        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
+//        Bank bank = new Bank("My Bank", dataSourceFile);
+
+        //spring
+        ApplicationContext context = new AnnotationConfigApplicationContext(ATMConfig.class);
+//        ATM atm = new ATM(bank);
+        AtmUI atmUI = context.getBean(AtmUI.class);
+        atmUI.run();
+    }
+}
